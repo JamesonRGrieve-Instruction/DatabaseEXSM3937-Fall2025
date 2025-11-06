05cb9df1e82c07fa1f39ffc1d3439868c9b235f8
James Grieve <jamesonrgrieve@gmail.com>
2025-11-05 19:34:19 -0700

    Create other tables;

--- 01-create-table.sql
+++ 01-create-table.sql
@@ -1,8 +1,44 @@
 -- database: ./my-database.db
+-- Create the user table.
+CREATE TABLE IF NOT EXISTS
+    user (
+        id INTEGER,
+        first_name TEXT,
+        last_name TEXT,
+        date_of_birth TEXT
+    );
 
-CREATE TABLE user (
-    id INTEGER,
-    first_name TEXT,
-    last_name TEXT,
-    date_of_birth TEXT
-);
+CREATE TABLE IF NOT EXISTS
+    role (id INTEGER, name TEXT);
+
+CREATE TABLE IF NOT EXISTS
+    course (
+        id INTEGER,
+        code TEXT,
+        name TEXT,
+        start_date TEXT,
+        end_date TEXT
+    );
+
+CREATE TABLE IF NOT EXISTS
+    section (
+        id INTEGER,
+        beginning_time TEXT,
+        duration_minutes INTEGER,
+        name TEXT,
+        course_id INTEGER
+    );
+
+CREATE TABLE IF NOT EXISTS
+    user_sections (
+        id INTEGER,
+        user_id INTEGER,
+        role_id INTEGER,
+        section_id INTEGER
+    );
+
+-- Show the names of all tables in the database.
+SELECT
+    name
+from
+    sqlite_master;
